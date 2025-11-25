ff38b1056a450166726fc86f2af058ea626e59d4
AgusJumaldin <muhamadagusjumaldin@gmail.com>
2025-11-26 02:36:45 +0700

    changing name from landing to homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
+import Home from "./pages/Home";
 import Index from "./pages/Scan";
 import BodyTypes from "./pages/BodyTypes";
 import HourglassBodyType from "./pages/HourglassBodyType";
@@ -22,7 +22,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route path="/" element={<Home />} />
           <Route path="/scan" element={<Index />} />
           <Route path="/body-types" element={<BodyTypes />} />
           <Route path="/body-types/hourglass" element={<HourglassBodyType />} />
